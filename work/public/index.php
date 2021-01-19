70af9d06bd430c733ecfdb5284902ed5eff7c3ec
shuyossy <shuyossy220@gmail.com>
2021-01-19 22:17:45 +0900

    first commit

---
+++ work/public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+  <body>
+    
+    <script src="js/main.js"></script>
+  </body>
+</html>
